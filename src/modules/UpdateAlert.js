ece95f9ca454c75b78106a9687ac82c8fce8ad40
Zohaib haider <zebihaider123@gmail.com>
2023-10-18 01:24:30 +0500

    changes

--- src/modules/UpdateAlert.js
+++ src/modules/UpdateAlert.js
@@ -4,9 +4,8 @@ const UpdateAlert = (props) => {
   let { shos } = props
   return (
     <>
-
-      <div className={`flex justify-center items-center w-[100%] h-12  bg-green-400 text-green-950 -translate-y-12 -translate-x-full ${shos && "translate-x-0"} transition-all duration-500 delay-200 ease-in-out`}>
-        <h3>Operation is Successfully completed! Note is now Updated in Your All Notes</h3>
+      <div className={` z-50 flex relative -top-[21rem] justify-center items-center w-[100%] h-12 bg-green-400 text-green-950 -translate-x-full ${shos && "translate-x-0"} transition-all duration-500 delay-200 ease-in-out`}>
+        <h3>Update Operation is Successfully completed! values are updated</h3>
       </div>
       {
 
